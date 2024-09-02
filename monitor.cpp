3539b566a1c73103cdfef92712879c470ee3445f
Arundas <51848726+arundas005@users.noreply.github.com>
2024-09-03 00:07:51 +0530

    lint errors resolved

--- monitor.cpp
+++ monitor.cpp
@@ -6,30 +6,27 @@
 #include <vector>
 #include <string>
 
-using std::cout, std::vector, std::pair, std::string, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+using std::cout, std::vector, std::pair, std::string, \
+std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-
-enum class VitalType
-{
+enum class VitalType {
   TEMPERATURE,
   PULSE_RATE,
   SPO2
 };
 
-vector<pair<float,float>> vitalLimits = {
+vector<pair<float, float>> vitalLimits = {
   {95.0f, 102.0f},
   {60.0f, 100.0f},
   {90.0f, 100.0f}
 };
 
-auto stringify(VitalType vitalTypes)
-{
+auto stringify(VitalType vitalTypes) {
   vector<string> vitalNames = {"Temperature", "Pulse Rate", "Oxygen Saturation"};
   return vitalNames[static_cast<int>(vitalTypes)];
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2)
-{
+int vitalsOk(float temperature, float pulseRate, float spo2) {
   vector<VitalType> vitals = {VitalType::TEMPERATURE, VitalType::PULSE_RATE, VitalType::SPO2};
   vector<float> vitalValues = {temperature, pulseRate, spo2};
   for (int i = 0; i < vitals.size(); i++)
@@ -43,13 +40,11 @@ int vitalsOk(float temperature, float pulseRate, float spo2)
   return 1;
 }
 
-bool vitalsInNormalRange(float value, float lowerLimit, float upperLimit)
-{
+bool vitalsInNormalRange(float value, float lowerLimit, float upperLimit) {
   return value >= lowerLimit && value <= upperLimit;
 }
 
-void alert(const string& vitalName)
-{
+void alert(const string& vitalName) {
   const int REPEAT_COUNT = 6;
   const int SLEEP_INTERVAL_SECONDS = 1;
   cout << vitalName << " is out of range!\n";
